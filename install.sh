3c55ed90c0b2dd2b4f0482b9f2c581ba9205ae6c
adib-enc <adib35785@gmail.com>
2023-07-04 16:49:37 +0700

    systemctl secret

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ echo "source ~/asis-lite/main.sh" >> ~/.bashrc
 source ~/asis-lite/main.sh
 cat ~/.bash_history | grep "docker " | sort | uniq > ~/asis-lite/sctdev
 cat ~/.bash_history | grep "cd " | sort | uniq > ~/asis-lite/sctdcd
+cat ~/.bash_history | grep "systemctl" | sort | uniq > ~/asis-lite/sctsyscx
